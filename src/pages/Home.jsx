8c04e4ab91c2584193be5cd9c61615422f1a3d52
Denis <denis.doda03@gmail.com>
2023-11-05 23:41:34 -0500

    Added keyframe animatinons to navbar and hero page, changed PNG of hero image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <>
       <Hero
         cName="hero"
-        image="https://images.unsplash.com/photo-1611858246382-da4877c6476d?auto=format&fit=crop&q=80&w=1032&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image="https://images.unsplash.com/photo-1617814086906-d847a8bc6fca?auto=format&fit=crop&q=80&w=2042&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Altitude Motors"
         text="Adrenaline Inducing Speed"
         btnText="Inventory"
