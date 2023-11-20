bafebd3f94c14cdab45862e055630feeece40bfe
Denis <denis.doda03@gmail.com>
2023-11-20 01:56:53 -0500

    reduced crammed text on mobile for models section

--- src/components/ModelExtend.js
+++ src/components/ModelExtend.js
@@ -7,19 +7,23 @@ class ModelExtend extends Component {
       <li class="model">
         <div className="model__wrapper">
           <img src={this.props.img} class="model__img" alt="Model Image" />
-          <div class="model__description">
-            <h2 class="model__description--title">{this.props.heading}</h2>
-            <p class="model__description--paragraph">{this.props.text}</p>
-            <p className="model__description--paragraph-cost">
-              From ${this.props.price}
-            </p>
-            <div className="model__description--button-wrapper">
-            <button className="model__description--button model__description--button-primary">
-                Build your {this.props.btntext}
-              </button>
-              <button className="model__description--button">
-                All {this.props.btntext} Models
-              </button>
+          <div className="model__description">
+            <div className="model__description--wrapper">
+              <h2 className="model__description--title">
+                {this.props.heading}
+              </h2>
+              <p className="model__description--paragraph">{this.props.text}</p>
+              <p className="model__description--paragraph-cost">
+                From ${this.props.price}
+              </p>
+              <div className="model__description--button-wrapper">
+                <button className="model__description--button model__description--button-primary">
+                  Build your {this.props.btntext}
+                </button>
+                <button className="model__description--button">
+                  All {this.props.btntext} Models
+                </button>
+              </div>
             </div>
           </div>
         </div>
